033157713e64e81db5b3f14962c802dbe8085700
Huy <huy@gohuygo.com>
2018-10-02 22:10:51 -0700

    Setup lottery class to handle method calls to contract

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
+  state = {
+    manager: ''
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager: manager })
+  }
+
   render() {
     return (
       <div className="App">
